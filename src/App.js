49a8ec8644dbd15540286a2ae4f09d1fbfd31116
Shreyas <shreyasmohite1203@gmail.com>
2022-02-06 19:26:06 +0530

    navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// Importing the navbar component inside
+// the main app file
+import Navbar from "./components/Navbar";
+
+const App = () =>{
+return (
+	<>
+	<Navbar/>		
+	</>
+);
+}
+
+export default App;
